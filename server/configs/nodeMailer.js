44f071695b6f0aa2fc2dd47369ee3fb71e9b15e8
Duy Bao <nguyenduybaodt1821@gmail.com>
2025-06-16 04:46:44 +0700

    fix

--- server/configs/nodeMailer.js
+++ server/configs/nodeMailer.js
@@ -3,7 +3,7 @@ const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
       type: "OAuth2",
-      user: "duybao11123@gmail.com",
+      user: process.env.SENDER_EMAIL,
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
