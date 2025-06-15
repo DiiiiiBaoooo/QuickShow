c377bd8aa9baffa78ab962a675c0683d3b892b71
Duy Bao <nguyenduybaodt1821@gmail.com>
2025-06-16 04:57:07 +0700

    a

--- server/configs/nodeMailer.js
+++ server/configs/nodeMailer.js
@@ -1,21 +1,36 @@
-import nodemailer from 'nodemailer'
+import nodemailer from 'nodemailer';
+import { google } from 'googleapis';
+
+const oAuth2Client = new google.auth.OAuth2(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  "https://developers.google.com/oauthplayground" // hoặc URI redirect lúc xin token
+);
+
+// set credentials từ refresh token
+oAuth2Client.setCredentials({ refresh_token: process.env.GOOGLE_REFRESH_TOKEN });
+
+const accessToken = await oAuth2Client.getAccessToken();
+
 const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      type: "OAuth2",
-      user: process.env.SENDER_EMAIL,
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
-    },
+  service: "gmail",
+  auth: {
+    type: "OAuth2",
+    user: process.env.SENDER_EMAIL,
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+    accessToken: accessToken.token,
+  },
+});
+
+const sendEmail = async ({ to, subject, body }) => {
+  const response = await transporter.sendMail({
+    from: process.env.SENDER_EMAIL,
+    to,
+    subject,
+    html: body,
   });
-  const sendEmail = async ({to,subject,body}) =>{
-            const response = await transporter.sendMail({
-                from:process.env.SENDER_EMAIL,
-                to,
-                subject,
-                html:body,
-            })
-            return response
-  }
+  return response;
+};
   export default sendEmail;
