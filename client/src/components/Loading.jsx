9daf13abab6c85ef2d964263485e23a24a665942
Duy Bao <nguyenduybaodt1821@gmail.com>
2025-06-19 03:21:09 +0700

    aa

--- client/src/components/Loading.jsx
+++ client/src/components/Loading.jsx
@@ -1,38 +1,41 @@
-```jsx
-import React from 'react';
+import React from "react";
 
 const Loading = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="loader"></div>
-      <style jsx>{`
-        .loader {
-          width: 120px;
-          height: 22px;
-          border-radius: 40px;
-          color: #514b82;
-          border: 2px solid;
-          position: relative;
-          overflow: hidden;
+    <div className="flex justify-center items-center h-[80vh]">
+      <div className="fancy-loader" />
+      <style>{`
+        .fancy-loader {
+          --s: 25px;
+          --g: 5px;
+
+          width: calc(2*(1.353*var(--s) + var(--g)));
+          aspect-ratio: 1;
+          background:
+            linear-gradient(#ff1818 0 0) left/50% 100% no-repeat,
+            conic-gradient(from -90deg at var(--s) calc(0.353*var(--s)),
+              #fff 135deg,#666 0 270deg,#aaa 0);
+          background-blend-mode: multiply;
+          --_m:
+            linear-gradient(to bottom right,
+              #0000 calc(0.25*var(--s)),#000 0 calc(100% - calc(0.25*var(--s)) - 1.414*var(--g)),#0000 0),
+            conic-gradient(from -90deg at right var(--g) bottom var(--g),#000 90deg,#0000 0);
+          -webkit-mask: var(--_m);
+          mask: var(--_m);
+          background-size: 50% 50%;
+          -webkit-mask-size: 50% 50%;
+          mask-size: 50% 50%;
+          -webkit-mask-composite: source-in;
+          mask-composite: intersect;
+          animation: l9 1.5s infinite;
         }
-        .loader::before {
-          content: "";
-          position: absolute;
-          margin: 2px;
-          width: 14px;
-          top: 0;
-          bottom: 0;
-          left: -20px;
-          border-radius: inherit;
-          background: currentColor;
-          box-shadow: -10px 0 12px 3px currentColor;
-          clip-path: polygon(0 5%, 100% 0, 100% 100%, 0 95%, -30px 50%);
-          animation: l14 1s infinite linear;
-        }
-        @keyframes l14 {
-          100% {
-            left: calc(100% + 20px);
-          }
+
+        @keyframes l9 {
+          0%,12.5%    {background-position:0% 0%,0 0}
+          12.6%,37.5% {background-position:100% 0%,0 0}
+          37.6%,62.5% {background-position:100% 100%,0 0}
+          62.6%,87.5% {background-position:0% 100%,0 0}
+          87.6%,100%  {background-position:0% 0%,0 0}
         }
       `}</style>
     </div>
@@ -40,4 +43,3 @@ const Loading = () => {
 };
 
 export default Loading;
-```
